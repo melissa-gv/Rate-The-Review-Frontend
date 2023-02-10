7ce4724d47458b34ff2ba5893ae6dbb126a7ac30
melissa-gv <melissagilv@gmail.com>
2023-02-10 15:50:50 -0600

    set chunkSizeWarningLimit: 1600 to vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
         manualChunks(id) {
