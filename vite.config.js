eae50eb37977660924f5a1f156eec330f8dc7a53
melissa-gv <melissagilv@gmail.com>
2023-02-10 15:38:09 -0600

    Updates vite config to chunk code

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    [react()],
+    [splitVendorChunkPlugin()],
+  ],
 })
