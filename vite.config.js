2350db6b9ce48f64f86ba857368442fda33bf9bf
melissa-gv <melissagilv@gmail.com>
2023-02-10 15:49:21 -0600

    Added manualChunks of node modules to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    [react()],
-    [splitVendorChunkPlugin()],
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+    },
+  },
 })
