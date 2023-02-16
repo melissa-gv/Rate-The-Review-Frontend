3c3557006833e5db072ddff3a4b3658a318e3569
melissa-gv <melissagilv@gmail.com>
2023-02-16 00:14:56 -0600

    Updates vite.config.js to remove lint error

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
+        // eslint-disable-next-line consistent-return
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString()
